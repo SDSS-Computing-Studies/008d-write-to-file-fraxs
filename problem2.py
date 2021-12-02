9206a635582fc4d83507624e5d76b2aebe512e06
fraxs <90715521+fraxs@users.noreply.github.com>
2021-12-02 14:40:43 -0800

    done

--- problem2.py
+++ problem2.py
@@ -7,11 +7,12 @@ Write a program to keep track of your current grades for 8 subjects.  Create a m
 """
 import json
 import os
+import time as t
 filename = "data.txt"
 def start():
     if os.path.isfile(filename) == False:
         file = open(filename,"w")
-        file.write('["SubjectA","GradeA","SubjectB","GradeB","SubjectC","GradeC","SubjectD","GradeD"]')
+        file.write('["SubjectA","GradeA","SubjectB","GradeB","SubjectC","GradeC","SubjectD","GradeD","SubjectE","GradeE","SubjectF","GradeF","SubjectG","GradeG","SubjectH","GradeH"]')
     else:
         fileRead = open(filename,"r")
         inputdata = fileRead.read()
@@ -21,9 +22,46 @@ def start():
 def firstFunc():
     x = input("Enter the subject you want to change: ")
     y = input("Enter what you want to change it to: ")
-    if x in list1:
-        list1.replace(x,y)
-        print(list1)
+    for i in range(len(list1)):
+        if x in list1[i]:
+            list1[i] = y
 
-start()
-firstFunc()
+def secondFunc():
+    x = input("Enter the subject you want to change the grade of: ")
+    y = input("Enter what you want to change the grade to: ")
+    for i in range(len(list1)):
+        if x in list1[i]:
+            list1[i+1] = y
+
+def thirdFunc():
+    print(f" SUBJECT:GRADES \n{list1[0],list1[1]}\n{list1[2],list1[3]}\n{list1[4],list1[5]}\n{list1[6],list1[7]}\n{list1[8],list1[9]}\n{list1[10],list1[11]}\n{list1[12],list1[13]}\n{list1[14],list1[15]}")
+    t.sleep(5)
+def fourthFunc():
+    x = json.dumps(list1)
+    fileWrite = open(filename,"w")
+    fileWrite.write(x)
+    print("File saved!")
+
+def close():
+    exit()
+
+
+def main():
+    start()
+    while True:
+        x = input("1. Change the name of a subject \n2. Change the grade for a subject \n3. Read subject data \n4. Save the current data \n5. Exit the program \n")
+        if x == '1':
+            firstFunc()
+        elif x == '2':
+            secondFunc()
+        elif x == '3':
+            thirdFunc()
+        elif x == '4':
+            fourthFunc()
+        elif x == '5':
+            close()
+        else:
+            print("Invalid input.")
+
+if __name__ == "__main__":
+    main()
